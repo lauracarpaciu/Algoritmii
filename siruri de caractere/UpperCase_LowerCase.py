9451d903b6fc34763066644a1429736cc2a84623
Mirela <lauracarpaciu>
2022-07-01 13:50:12 +0300

    UpperLower

---
+++ siruri de caractere/UpperCase_LowerCase.py
@@ -0,0 +1,10 @@
+destination = "bucuRESti"
+
+primaLitera = destination[0]
+urmatoareleLitere = destination[1:len(destination)]
+
+
+upper=primaLitera.capitalize()
+lower = urmatoareleLitere.lower()
+concat = upper + lower
+print("Am rescris stingul destination :{} ".format(concat))
