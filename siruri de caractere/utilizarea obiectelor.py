cb97f7ea6aa74b05091ce6abce668cec4a403e16
Mirela <lauracarpaciu>
2022-07-11 15:27:33 +0300

    utilizarea obiecetlor ne permite noua sa accesam atributele fiecarui obiect din lista de obiecte, si sa il afisam, sau sa evaluam!

---
+++ siruri de caractere/utilizarea obiectelor.py
@@ -0,0 +1,31 @@
+#
+# nume = ["Popescu","Georgescu","Ioescu"]
+# ages = [26,35,29]
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Ionescu", 32)
+print(p1.name)
+
+persons = [
+  (
+    "Ion",
+    36
+  ),
+    ("Ion", 63),
+    ("Ion",29 ),
+]
+persons.append( ("Ion",29 ))
+
+for p in persons:
+    for i in p:
+        print(i)
+    try:
+        i = int(i)
+        if i <= 29:
+            print ("Ion are :{}".format(i))
+    except:
+        ValueError()
