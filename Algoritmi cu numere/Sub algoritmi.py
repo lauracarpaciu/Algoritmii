e6ce1f215fa101303879b0e5c301c0749eceb9ae
Mirela <lauracarpaciu>
2022-08-17 13:49:28 +0300

    sub algoritmi

---
+++ Algoritmi cu numere/Sub algoritmi.py
@@ -0,0 +1,18 @@
+def calculatePriceByTrain(distance):
+    cost = distance*1
+    return cost
+
+def calculatePriceByPlain(distance):
+    cost = distance*0.5+200
+    return cost
+
+def getCheaperOption(distance):
+    costByTrain=calculatePriceByTrain(distance)
+    costByPlain = calculatePriceByPlain(distance)
+
+    if costByTrain<costByPlain:
+        print("Train")
+    else:
+        print("Plain")
+
+getCheaperOption(350)        
